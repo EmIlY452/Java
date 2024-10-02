7abc8edf81a15cd9ff36a6290fd20ef361ab1fde
EmIlY452 <142310396+EmIlY452@users.noreply.github.com>
2024-10-02 09:46:33 -0300

    Add files via upload

---
+++ projetodeDomicio/script.js
@@ -0,0 +1,9 @@
+const toggleButton = document.getElementById('theme-toggle');
+const body = document.body;
+
+toggleButton.addEventListener('click', () => {
+    body.classList.toggle('dark-theme');
+    body.classList.toggle('light-theme');
+});
+
+
